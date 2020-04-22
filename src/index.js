3a5f32efe9b5efbd38eacf97a983f842d6111e1a
Bastian Bernst <bbernst@uni-koblenz.de>
2020-04-22 20:54:30 +0200

    Switch to QuoteMachine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class RandomQuoteGenerator extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      <QuoteWrapper />
+    )
+  }
+
+}
+
+function Quote (props) {
+  return (
+    <p id={'text'}>"{props.children}"</p>
+  )
+}
+
+function Author (props) {
+  return (
+    <p id={'author'}>- {props.children}</p>
+  )
+}
+function Button(props) {
+  return (
+    <button id={props.id} onClick={props.onClick}>{props.children}</button>
+  )
+}
+
+function QuoteWrapper(props) {
+  return (
+    <div id={'quote-box'}>
+      <Quote>LOREM IPSUM DOLOR SIT AMET.</Quote>
+      <Author>HANS MEISER</Author>
+      <Button id={'new-quote'}>New Quote</Button>
+      <a href={'#'} id={'tweet-quote'}>Tweet!</a>
+    </div>
+
+  )
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <RandomQuoteGenerator/>,
+  document.getElementById('root'),
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
