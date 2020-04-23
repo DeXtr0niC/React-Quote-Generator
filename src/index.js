2747dfd4f3270f65ad5dd09d46a9fbb353a8d1c3
Bastian Bernst <bbernst@uni-koblenz.de>
2020-04-23 18:41:20 +0200

    Add animations and handle loading

--- src/index.js
+++ src/index.js
@@ -2,76 +2,138 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.scss'
 
+// ==== CONSTANTS ====
 
-// MAIN COMPONENT
+const QUOTEAPI = "https://programming-quotes-api.herokuapp.com/quotes/random"
+
+// ==== MAIN COMPONENT ====
+
 class RandomQuoteGenerator extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      currentQuote: 'In order to succeed, your desire for success should be greater than your fear of failure.' ,
-      currentAuthor: 'default author'
+      currentQuote: '1010001001 1101101 11010 101 0' ,
+      currentAuthor: 'default author',
+      isLoading: false,
+      error: null
     }
     this.fetchNewQuote = this.fetchNewQuote.bind(this)
+    this.handleNewQuote = this.handleNewQuote.bind(this)
+  }
+
+  componentDidMount () {
+    this.fetchNewQuote()
+  }
+
+  handleNewQuote() {
+    this.setState({isLoading: true})
+    this.fetchNewQuote()
   }
 
   fetchNewQuote = () => {
-    fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
-    .then(res => res.json())
+    fetch(QUOTEAPI)
+    .then(res => {
+      if(res.ok) {
+        return res.json()
+      } else {
+        throw new Error('Could not fetch new quote.')
+      }
+    })
     .then(
-      (result) => this.setState({currentQuote: result.en, currentAuthor: result.author})
+      (result) => this.setState({currentQuote: result.en, currentAuthor: result.author, isLoading: false})
     )
+      .catch(error => this.setState({error, isLoading: false}))
   }
 
   render () {
+    const {currentQuote, currentAuthor, isLoading, error} = this.state
+
+    if(error) {
+      return (<p>{error.message}</p>)
+    }
+
     return (
-      <QuoteWrapper newQuoteHandler={this.fetchNewQuote} quote={this.state.currentQuote} author={this.state.currentAuthor}/>
+      <QuoteWrapper isLoading={isLoading} newQuoteHandler={this.handleNewQuote} quote={currentQuote} author={currentAuthor}/>
     )
   }
 
 }
 
-// CONTAINER COMPONENT
+// ==== CONTAINER COMPONENTS ====
+
 function QuoteWrapper(props) {
+  if(props.isLoading) {
+    return (
+      <div id={'quote-box'}>
+        Loading...
+      </div>
+    )
+  }
   return (
     <div id={'quote-box'}>
-      <Quote>{props.quote}</Quote>
-      <Author>{props.author}</Author>
+        <Quote>{props.quote}</Quote>
+        <Author>{props.author}</Author>
       <ButtonBar>
         <TwitterButton currentQuote={[props.quote, props.author]}/>
         <NewQuoteButton clickHandler={props.newQuoteHandler}/>
       </ButtonBar>
     </div>
-
   )
 }
-// SMALLER STATELESS COMPONENTS
+
+let ButtonBar = (props) => <div id={'buttonbar'}>{props.children}</div>
+
+// ==== TEMPLATE COMPONENTS ====
+
+let IconButton = (props) => <div {...props} className={'button'} >{props.children}</div>
+
+class FadingComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      fade: false
+    }
+  }
+
+  componentDidMount () {
+    this.setState({fade: true})
+  }
+
+  render () {
+    const fade = this.state.fade
+    return (
+      <div className={fade ? 'fade' : ''} onAnimationEnd={() => this.setState({fade: false})}>{this.props.children}</div>
+    )
+  }
+
+}
+
+// ==== SMALLER STATELESS COMPONENTS ====
 let Quote  = (props) => (
-  <div className={'quote-text'}>
-    <i className="fas fa-quote-left"></i>
-    <span id={'text'}>{props.children}</span>
-    <i className="fas fa-quote-right"></i>
-  </div>
+  <FadingComponent fade={props.fade}>
+    <div className={'quote-text'}>
+     <i className="fas fa-quote-left"></i>
+      <span id={'text'}>{props.children}</span>
+     <i className="fas fa-quote-right"></i>
+    </div>
+  </FadingComponent>
 )
 
 let Author = (props) => (
-  <div className={'quote-author'}>
-    - <span id={'author'}>{props.children}</span>
-  </div>
+  <FadingComponent>
+   <div className={'quote-author'}>
+     - <span id={'author'}>{props.children}</span>
+   </div>
+  </FadingComponent>
 )
 
-let ButtonBar = (props) => (
-  <div id={'buttonbar'}>{props.children}</div>
-)
 
-let IconButton = (props) => (
-  <div {...props} className={'button'} >{props.children}</div>
-)
 
 function TwitterButton(props) {
   let uri = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + props.currentQuote[0] + '" ' + props.currentQuote[1])
   return (
     <IconButton id={'twitter-button'} title={'Tweet this quote!'}>
-      <a id={'tweet-quote'} target={'_blank'} href={uri}> <i className="fab fa-twitter"></i></a>
+      <a id={'tweet-quote'} target={'_blank'}  rel="noopener noreferrer" href={uri}> <i className="fab fa-twitter"></i></a>
     </IconButton>
   )
 }
